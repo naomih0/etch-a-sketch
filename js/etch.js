2961a1f174bca5cf1eba44ac2fabe4196207d4c8
naomih0 <123221320+naomih0@users.noreply.github.com>
2024-02-18 19:01:55 -0600

    Added the ability to change cell colors randomly on mouseover.

--- js/etch.js
+++ js/etch.js
@@ -16,4 +16,25 @@ function createGrid(num) {
     };
 };
 
-createGrid(8)
+createGrid(16)
+
+const cellColor = document.querySelectorAll('.cell')
+
+function randomColor() {
+    return Math.floor(Math.random() * 256);
+}
+
+function changeColor(event) {
+    let r = randomColor()
+    let g = randomColor()
+    let b = randomColor()
+    let a = ((Math.random() * 1).toFixed(1))
+    
+    let rgba = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')'
+    event.target.style.backgroundColor = rgba;
+}
+
+cellColor.forEach(cell => {
+    cell.addEventListener('mouseover', changeColor);
+}) 
+
